17ee2d5c577855e00d365f4f24e745a7f2e04e7c
Luis Sánchez <luis.sanchezm86@gmail.com>
2020-09-17 15:54:52 -0300

    Updates on the creation menu class view

--- norawebapp/urls.py
+++ norawebapp/urls.py
@@ -3,6 +3,6 @@ from norawebapp import views
 
 urlpatterns = [
     path("", views.index, name="index"),
-    path("menu/create", views.createMenu, name="createmenu"),
+    path("menu/create/", views.MenuFormView.as_view(), name="createmenu"),
     path("menu/<uuid:id>", views.Menu.as_view(), name="menu"),
 ]
--- norawebapp/views.py
+++ norawebapp/views.py
@@ -3,37 +3,13 @@ from norawebapp.forms import MenuForm
 from norawebapp.models import Menu as MenuModel
 from rest_framework.views import APIView
 
+
 def index(request):
     return render(request, "norawebapp/index.html")
 
-def createMenu(request):
-    form = MenuForm(request.POST or None)
-    menu_instance = MenuModel()
-
-    if request.method == "POST":
-        if form.is_valid():
-            menu_instance.option_one = form.cleaned_data['option_one']
-            menu_instance.option_two = form.cleaned_data['option_two']
-            menu_instance.option_three = form.cleaned_data['option_three']
-            menu_instance.option_four = form.cleaned_data['option_four']
-            menu_instance.date = form.cleaned_data['date']
-            menu_instance.save()
-            
-            # need to check if the menu exists for that date
-            # message = "El menú ya existe para ese día"
-            # return render(request, "norawebapp/createMenu.html", {"form": form, "message": message})
-
-            context = {
-                "message": "Menu ingresado!",
-            }
-            return render(request, "norawebapp/index.html", context)
-        else:
-            render(request, "norawebapp/createMenu.html")
-    else:
-        return render(request, "norawebapp/createMenu.html", {"form": form})
 
 class Menu(APIView):
-    """ Retrieve menu of the day by its uuid """
+    ''' Retrieve menu of the day by its uuid '''
     def get(self, request, **kwargs):
         id = kwargs.get('id', None)
         if id == None:
@@ -50,3 +26,37 @@ class Menu(APIView):
             "op4": menu_instance.option_four,
         }
         return render(request, "norawebapp/menu.html", context)
+
+class MenuFormView(APIView):
+    form_class = MenuForm
+    template_name = "norawebapp/createMenu.html"
+
+    def get(self, request):
+        form = self.form_class(request.GET or None)
+        return render(request, self.template_name, {'form': form})
+
+    ''' Creates the menu '''
+    def post(self, request, *args, **kwargs):
+        form = self.form_class(request.POST or None)
+        menu_instance = MenuModel()
+        
+        if form.is_valid():
+            menu_instance.option_one = form.cleaned_data['option_one']
+            menu_instance.option_two = form.cleaned_data['option_two']
+            menu_instance.option_three = form.cleaned_data['option_three']
+            menu_instance.option_four = form.cleaned_data['option_four']
+            menu_instance.date = form.cleaned_data['date']
+            menu_instance.save()
+
+            # need to check if the menu exists for that date
+            # message = "El menú ya existe para ese día"
+            # return render(request, "norawebapp/createMenu.html", {"form": form, "message": message})
+
+            context = {
+                "message": "Menu ingresado!",
+            }
+            return render(request, "norawebapp/index.html", context)
+        else:
+            render(request, self.template_name)
+
+        return render(request, "norawebapp/index.html")
