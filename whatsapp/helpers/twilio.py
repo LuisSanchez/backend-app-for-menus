ada2e9bab27fd113837c165788c3e86a8456a389
Luis Sánchez <luis.sanchezm86@gmail.com>
2020-09-17 15:53:51 -0300

    Minor updates on twilio broadcast

--- whatsapp/helpers/twilio.py
+++ whatsapp/helpers/twilio.py
@@ -3,19 +3,18 @@ from django.conf import settings
 from norawebapp.models import Menu
 
 
-def sendTestMessage(menu, numbers, url):
+def sendTestMessage(menu: Menu, numbers, url):
     account_sid = settings.TWILIO_ACCOUNT_SID
     auth_token = settings.TWILIO_AUTH_TOKEN
     client = Client(account_sid, auth_token) 
     
     menu_of_the_day = (f"Hola!\n"
                        f"Dejo el menú de hoy :)\n"
-                       f"Opción 1: {menu['option_one']}\n"
-                       f"Opción 2: {menu['option_two']}\n"
-                       f"Opción 3: {menu['option_three']}\n"
-                       f"Opción 4: {menu['option_four']}\n"
+                       f"Opción 1: {menu.option_one}\n"
+                       f"Opción 2: {menu.option_two}\n"
+                       f"Opción 3: {menu.option_three}\n"
+                       f"Opción 4: {menu.option_four}\n"
                        f"Tengan lindo día!")
-
 
     message = client.messages.create( 
                                 from_=settings.TWILIO_FROM_WHATSAPP,  
