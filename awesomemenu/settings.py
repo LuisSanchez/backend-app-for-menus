4d8028de3845c82c699d00ba984230b68157d30c
Luis Sánchez <luis.sanchezm86@gmail.com>
2020-09-23 11:17:02 -0300

    Added host for heroku

--- awesomemenu/settings.py
+++ awesomemenu/settings.py
@@ -27,7 +27,9 @@ SECRET_KEY = os.environ.get('SECRET_KEY_MENU', os.getenv("SECRET_KEY_MENU"))
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'nora-luissanchez.herokuapp.com',
+]
 
 
 # Application definition
